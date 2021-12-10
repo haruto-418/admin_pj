e51961bf08aae5c0383414374392c8b94a7a23dc
KikuchiHaruto <o5o206felt@icloud.com>
2021-12-10 17:03:39 +0900

    testのモデルを作った。

--- api/test_api.py
+++ api/test_api.py
@@ -2,5 +2,14 @@ import pytest
 from main import create_random_user
 
 
-def test_create_random_user():
-    pass
+class TestCreate(object):
+    @classmethod
+    def setup_class(cls):
+        print('start')
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+
+    def test_create_random_user():
+        pass
