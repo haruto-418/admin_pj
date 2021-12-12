be7724a7fd2aed0e72722dc22dd3c26996f3b97e
KikuchiHaruto <o5o206felt@icloud.com>
2021-12-12 08:23:36 +0000

    functions.pyにcreate_random_strings()とextract_from_file()を作った。

--- api/common/functions.py
+++ api/common/functions.py
@@ -1,4 +1,6 @@
 from typing import List
+import random
+import string
 
 def read_file(path:str,arr:List[str])->None:
     """
@@ -10,6 +12,29 @@ def read_file(path:str,arr:List[str])->None:
             if not line:
                 break
             arr.append(line)
+
+def create_random_strings(is_digit:bool,characters:int):
+    empty_str:str=""
+    if is_digit:
+        return empty_str.join([random.choice(string.ascii_letters+string.digits) for _ in range(characters)])
+    else:
+        return empty_str.join([random.choice(string.ascii_letters) for _ in range(characters)])
+
+def extract_from_file(path:str)->None:
+    """
+    外部ファイルを読み込み、配列に組み込む関数。
+    """
+    arr=[]
+    with open (path,'r')as f:
+        while True:
+            line=f.readline()
+            if not line:
+                break
+            arr.append(line)
+    return random.choice(arr)
+
+
     
 if __name__=='__main__':
-    read_file()
+    a=create_random_strings(True,6)
+    print(a)
