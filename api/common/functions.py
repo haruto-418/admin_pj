7c062aeef140ed2d549704088c9eaa252d52a116
KikuchiHaruto <o5o206felt@icloud.com>
2021-12-12 08:07:15 +0000

    common/fuctions.pyでListをimportした。

--- api/common/functions.py
+++ api/common/functions.py
@@ -1,13 +1,15 @@
+from typing import List
+
 def read_file(path:str,arr:List[str])->None:
     """
     外部ファイルを読み込み、配列に組み込む関数。
     """
     with open (path,'r')as f:
         while True:
-            line=readline()
+            line=f.readline()
             if not line:
                 break
             arr.append(line)
     
-if __name__='__main__':
+if __name__=='__main__':
     read_file()
