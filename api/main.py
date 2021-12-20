dfdc235a798876fa5e90bb194d38018ce0ccf225
KikuchiHaruto <o5o206felt@icloud.com>
2021-12-20 09:07:50 +0000

    オーダー追加関数のforループの回数を一回にした

--- api/main.py
+++ api/main.py
@@ -16,17 +16,15 @@ except ModuleNotFoundError:
     from .common import models
     from .common import type_classes
 
-if __name__ == 'api.main':
-    app: FastAPI = FastAPI()
-    firebase_admin.initialize_app()
-    db: firestore = firestore.client()
+
+app: FastAPI = FastAPI()
+firebase_admin.initialize_app()
+db: firestore = firestore.client()
 
 
 @app.post('/users')
 async def create_random_user(how_many_users: int) -> dict:
-    """
-    ユーザーを指定の人数作成する関数。
-    """
+    """ユーザーを指定の人数作成する関数。"""
     try:
         for _ in range(how_many_users):
             name: str = names.get_first_name()
@@ -46,9 +44,7 @@ async def create_random_user(how_many_users: int) -> dict:
 
 @app.delete('/users')
 async def delete_user(how_many_users: int) -> dict:
-    """
-    指定した人数分、ユーザーを削除する。
-    """
+    """指定した人数分、ユーザーを削除する。"""
     user_ref: firestore.CollectionReference = db.collection('users')
     try:
         for _ in range(how_many_users):
@@ -75,30 +71,20 @@ async def delete_all_users() -> dict:
 
 
 @app.post('/orders')
-def add_order(how_many_orders: int) -> None:
-    """
-    ユーザーを取得して、ID等を読み取り、それに基づいたオーダーを追加する。
-    """
-    def fetch_user_from_firestore(how_many_users: int, doc_ref: firestore.CollectionReference, arr: List[str]) -> None:
-        users: Generator = doc_ref.stream()
-        for _ in range(how_many_users):
-            user: firestore.DocumentSnapshot = next(users)
-            user_data: dict = user.to_dict()
-            arr.append([user.id, user_data['address']])
-
-    user_ref: firestore.CollectionReference = db.collection('users')
-    user_list: List[List[str]] = []
-
-    fetch_user_from_firestore(how_many_orders, user_ref, user_list)
-
+def add_order(how_many_orders: int) -> dict:
+    """オーダーを指定の個数作成する。"""
+    users = db.collection('users').stream()
+    order_strings: List[str] = []
+    functions.File.read_file(
+        '/src/api/assets/order_strings.csv', order_strings)
     try:
-        order_strings: List[str] = []
-        functions.read_file('/src/api/assets/order_strings.csv', order_strings)
-        for i in range(how_many_orders):
+        for _ in range(how_many_orders):
+            user_doc = next(users)
+            user_dict = user_doc.to_dict()
             db.collection('orders').add({
                 'createAt': firestore.SERVER_TIMESTAMP,
-                'customerId': user_list[i][0],
-                'deliveryAddress': user_list[i][1],
+                'customerId': user_doc.id,
+                'deliveryAddress': user_dict['address'],
                 'deliveryCharge': 0,
                 'deliveryPoint': '',
                 'maxQuotationPrice': 0,
@@ -107,7 +93,6 @@ def add_order(how_many_orders: int) -> None:
                 'text': random.choice(order_strings)
             })
         return {'200': 'success!'}
-
     except TypeError:
         return {'error': 'There are not enough amount of users.'}
     except Exception as e:
